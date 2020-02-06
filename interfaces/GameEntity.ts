8310d9f1ac210aaa29238f9eda5b5f24007363c6
Craig Fisk <fisk.craig@gmail.com>
2020-02-06 10:55:12 +0000

    Added box interface

--- interfaces/GameEntity.ts
+++ interfaces/GameEntity.ts
@@ -1,15 +1,11 @@
 import FiskGame from "../classes/Game";
+import Box from "./box";
 
 export default interface GameEntity {
 	x: number;
 	y: number;
 	width: number;
 	height: number;
-	hitbox?: {
-		x: number,
-		y: number,
-		width: number,
-		height: number
-	}
+	hitbox?: Box
 	setup?: (game:FiskGame) => void;
 }
