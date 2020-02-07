6e015612db915d59e30d18ed32549a2fdd86dcba
Craig Fisk <fisk.craig@gmail.com>
2020-02-07 23:42:45 +0000

    Updated game entity to support having polygon instead of width and height

--- interfaces/GameEntity.ts
+++ interfaces/GameEntity.ts
@@ -4,8 +4,9 @@ import Box from "./box";
 export default interface GameEntity {
 	x: number;
 	y: number;
-	width: number;
-	height: number;
+	width?: number;
+	height?: number;
+	polygon?: {x: number, y: number}[];
 	hitbox?: Box
 	setup?: (game:FiskGame) => void;
 }
