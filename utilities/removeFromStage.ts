23f69d0043f692625ae6b587282f634a5e8e1c98
Craig Fisk <fisk.craig@gmail.com>
2023-04-18 16:48:24 +0100

    fix remove from stage to remove from interactors list

--- utilities/removeFromStage.ts
+++ utilities/removeFromStage.ts
@@ -1,32 +1,42 @@
-import { FiskGame } from '..';
-import { GameStage } from '..';
-import Collidable from '../interfaces/Collidable';
-import GameEntity from '../interfaces/GameEntity';
-import Logical from '../interfaces/Logical';
-import Renderable from '../interfaces/Renderable';
+import { FiskGame } from "..";
+import { GameStage } from "..";
+import Collidable from "../interfaces/Collidable";
+import GameEntity from "../interfaces/GameEntity";
+import Interactable from "../interfaces/Interactable";
+import Logical from "../interfaces/Logical";
+import Renderable from "../interfaces/Renderable";
 
 function isEngine(entity: GameStage | FiskGame): entity is FiskGame {
-    return (<FiskGame>entity).currentStage !== undefined;
+  return (<FiskGame>entity).currentStage !== undefined;
 }
 
-export default function removeFromStage(entity: GameEntity, gameStage: GameStage | FiskGame) {
-    const stage = isEngine(gameStage) ? gameStage.currentStage as GameStage : gameStage;
-    
-    const entityIndex = stage.entities.indexOf(entity);
-    const logicIndex = stage.logicQueue.indexOf(entity as Logical);
-    const renderIndex = stage.renderQueue.indexOf(entity as Renderable);
-    const collisionIndex = stage.collisionQueue.indexOf(entity as Collidable);
+export default function removeFromStage(
+  entity: GameEntity,
+  gameStage: GameStage | FiskGame
+) {
+  const stage = isEngine(gameStage)
+    ? (gameStage.currentStage as GameStage)
+    : gameStage;
 
-    if(entityIndex >= 0) {
-        stage.entities.splice(entityIndex, 1);
-    }
-    if(logicIndex >= 0) {
-        stage.logicQueue.splice(logicIndex, 1);
-    }
-    if(renderIndex >= 0) {
-        stage.renderQueue.splice(renderIndex, 1);
-    }
-    if(collisionIndex >= 0) {
-        stage.collisionQueue.splice(collisionIndex, 1);
-    }
+  const entityIndex = stage.entities.indexOf(entity);
+  const logicIndex = stage.logicQueue.indexOf(entity as Logical);
+  const renderIndex = stage.renderQueue.indexOf(entity as Renderable);
+  const collisionIndex = stage.collisionQueue.indexOf(entity as Collidable);
+  const interactorsIndex = stage.interactors.indexOf(entity as Interactable);
+
+  if (entityIndex >= 0) {
+    stage.entities.splice(entityIndex, 1);
+  }
+  if (logicIndex >= 0) {
+    stage.logicQueue.splice(logicIndex, 1);
+  }
+  if (renderIndex >= 0) {
+    stage.renderQueue.splice(renderIndex, 1);
+  }
+  if (collisionIndex >= 0) {
+    stage.collisionQueue.splice(collisionIndex, 1);
+  }
+  if (interactorsIndex >= 0) {
+    stage.interactors.splice(interactorsIndex, 1);
+  }
 }
